1f4a5caaa156b1c59fa2ba1a3ea8dbb842b7319c
Greasytugboat6 <78366801+Greasytugboat6@users.noreply.github.com>
2023-11-03 23:15:42 -0500

    fix

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,12 +1,14 @@
 
-let accelReader = document.getElementsByClassName("accelReader");
+accelReader = document.getElementsByID("accelReader");
 function getCadence(){
     console.log("JS loaded")
+    accelReader.innerHTML = "HTML modification works";
     DeviceMotionEvent.requestPermission().then(response => {
         if (response == 'granted') {
             console.log("accelerometer permission granted");
             window.addEventListener('devicemotion', (event) => {
                 var netAccel = event.acceleration
+                
                 
                 accelReader.innerHTML = netAccel
             })
